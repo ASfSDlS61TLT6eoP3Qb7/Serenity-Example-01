387944151203ff1c65c3aa0217566b22d91b6115
Mark Henneman <mhenneman@gmail.com>
2017-05-02 23:03:45 +0200

    My first Serenity reports!!

--- src/test/java/MyFirstSerenityTest.java
+++ src/test/java/MyFirstSerenityTest.java
@@ -1,5 +1,8 @@
+import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SerenityRunner.class)
 public class MyFirstSerenityTest {
 
     @Test
