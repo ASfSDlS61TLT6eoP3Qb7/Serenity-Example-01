db6cae3963f8041af4b99d8d28586e16636017bb
Mark Henneman <mark@ambrero.nl>
2017-05-03 09:32:02 +0200

    Added pending testCase

--- src/test/java/MyFirstSerenityTest.java
+++ src/test/java/MyFirstSerenityTest.java
@@ -1,4 +1,5 @@
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Pending;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -9,4 +10,7 @@ public class MyFirstSerenityTest {
     public void testCase001() {
         System.out.println("Hello World!");
     }
+
+    @Pending
+    public void testCase002() {}
 }
